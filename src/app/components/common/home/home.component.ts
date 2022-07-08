2cd076ee8a6c9d948ed9658fe7e204c284b7445a
konzerra <konzerra@gmail.com>
2022-07-08 08:12:57 +0600

    sync

--- src/app/components/common/home/home.component.ts
+++ src/app/components/common/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {DialogsService} from "../dialogs/dialogs.service";
+import {FormControl, Validators} from "@angular/forms";
+import {Product} from "../../../domain/product/Product";
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,9 @@ import {DialogsService} from "../dialogs/dialogs.service";
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  products = new Array<Product>()
+  selectedProduct = new FormControl<Product | null>(null)
 
   constructor(
     private dialogsService:DialogsService
---
+++ src/app/domain/product/Product.ts
@@ -0,0 +1,12 @@
+
+export interface Product {
+  id: Number,
+  active: boolean,
+  min_sum: Number,
+  max_sum: Number,
+  min_duration: Number,
+  max_duration: Number,
+  percent: Number,
+  name: string
+
+}
